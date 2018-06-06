9db40d8a22a14057a0012c4b782c2c2be44a8e61
Dominic Eggerman <dominiceggerman@gmail.com>
2018-06-06 11:34:57 -0400

    Commit before debug

---
+++ scripts/script.js
@@ -0,0 +1,10 @@
+/* ---------------------------------
+Scripts for Jeffrey Kenney's Website
+--------------------------------- */
+
+// M86 H-alpha toggling
+$(document).ready(function() {
+    $("#cf_onclick").click(function() {
+    $("#cf2 img.top").toggleClass("transparent");
+  });
+  });
