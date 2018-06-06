fe3a06fba67997b6af6d9eba0debc5b516e39dc4
Dominic Eggerman <dominiceggerman@gmail.com>
2018-06-06 15:47:16 -0400

    worked on image grid and nav bar.

--- scripts/script.js
+++ scripts/script.js
@@ -3,8 +3,7 @@ Scripts for Jeffrey Kenney's Website
 --------------------------------- */
 
 // M86 H-alpha toggling
-$(document).ready(function() {
-    $("#cf_onclick").click(function() {
-    $("#cf2 img.top").toggleClass("transparent");
-  });
-  });
+function toggleImage() {
+    // Get top image and add/remove the hide-img class
+    document.getElementById("m86-top").classList.toggle("hide-img")
+}
