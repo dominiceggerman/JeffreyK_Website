260536ee99dfdcbf4dba2e7741d8228fb6e3a13b
Dominic Eggerman <dominiceggerman@gmail.com>
2018-06-06 18:10:24 -0400

    Started styling. Added zoom boxes.

--- scripts/script.js
+++ scripts/script.js
@@ -2,8 +2,14 @@
 Scripts for Jeffrey Kenney's Website
 --------------------------------- */
 
-// M86 H-alpha toggling
+/* -----------------
+M86 H-alpha toggling
+----------------- */
 function toggleImage() {
-    // Get top image and add/remove the hide-img class
-    document.getElementById("m86-top").classList.toggle("hide-img")
+    // Get top image and add/remove the hidden class
+    document.getElementById("m86-top").classList.toggle("hidden")
 }
+
+/* ---------------
+NGC 4921 modal box
+--------------  */
